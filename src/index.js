b5029112238abca2a42812aeb2b6dac81d87952d
Chris Adams <chris@productscience.co.uk>
2019-08-04 11:35:24 +0200

    API calls return data!

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const AWS = require("aws-sdk")
+
+async function main() {
+
+  const creds = {
+    apiVersion: '2017-10-25',
+    region: 'us-east-1',
+  }
+
+  const costExplorer = new AWS.CostExplorer(creds);
+
+  const ceParams = {
+    // Start=2018-08-01,End=2019-05-01
+    TimePeriod: { /* required */
+      Start: '2018-08-01', /* required */
+      End: '2019-05-01' /* required */
+    },
+    Granularity: "MONTHLY",
+    Metrics: [
+      "BlendedCost",
+      "UsageQuantity"
+    ],
+    GroupBy: [{
+      Key: 'AZ',
+      Type: "DIMENSION"
+    }]
+  }
+  // give us an awaitable function
+  async function getCosts() {
+    return new Promise(function (resolve, reject) {
+      costExplorer.getCostAndUsage(ceParams, function (err, data) {
+        if (err) {
+          reject(err)
+        }
+        console.log({ data })
+        resolve(data)
+      })
+    })
+  }
+  const result = await getCosts()
+  console.log(result)
+
+
+}
+
+main()
