86b712f0588b72a07168640fa476c481d86b664a
Franziska Schmidt <franka.schmidt@mapbox.com>
2019-08-13 16:17:12 +0200

    Format cost data, add new test.
    Co-authored-by: Chris Adams <chris@productscience.co.uk>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const AWS = require("aws-sdk")
+require('dotenv').config();
 
 const creds = {
   apiVersion: '2017-10-25',
@@ -36,18 +37,96 @@ async function getRawCosts() {
   })
 }
 
+function mapToJson(map) {
+    return JSON.stringify([...map]);
+}
+
+/** Given the output of `getAssignedCost` (see sampledata.json for a sample)
+ *  returns green vs grey cost.
+ *  Such as:
+ *  { greenCost: 123, greyCost: 123, greenPercent: 20, greyPercent: 80 }
+ */
+function getTotalCost(groupedCost) {
+
+  const costPerRegion = {};
+
+  for (const [key, value] of groupedCost.entries()) {
+
+    const greenReducer = (accumulator, currentValue) => {
+      if (currentValue.greenRegion) {
+        return (accumulator + currentValue.blendedCost);
+      } else {
+        return accumulator;
+      }
+    };
+
+    const grayReducer = (accumulator, currentValue) => {
+     if (!currentValue.greenRegion) {
+        return (accumulator + currentValue.blendedCost);
+      } else {
+        return accumulator;
+      }
+    };
+
+    const greenSum = value.reduce(greenReducer, 0);
+    const greySum = value.reduce(grayReducer, 0);
+
+    costPerRegion[key] = { greenCost: greenSum, greyCost: greySum};
+  };
+
+  let totalGreenCost = 0;
+  let totalGreyCost = 0;
+
+  Object.values(costPerRegion).forEach((region) => {
+    totalGreenCost = totalGreenCost + region.greenCost;
+    totalGreyCost = totalGreyCost + region.greyCost;
+  })
+
+  const greenPercent = totalGreenCost / (totalGreyCost + totalGreenCost) * 100;
+  const greyPercent = 100 - greenPercent;
+
+  return { greenCost: totalGreenCost.toFixed(2), greyCost: totalGreyCost.toFixed(2), greenPercent: greenPercent.toFixed(1), greyPercent: greyPercent.toFixed(1) };
+}
+
+function sumByRegion(costArray) {
+  // Group by region
+  const groupedCost = new Map();
+  costArray.forEach((item) => {
+    const key = item.region;
+    const collection = groupedCost.get(key);
+    if (!collection) {
+      groupedCost.set(key, [item]);
+    } else {
+      collection.push(item);
+    }
+  });
+
+  return groupedCost;
+}
+
+/**
+ * Given the raw output of CostExplorer.getCostAndUsage(), decorates the data with `greenRegion: true/false` and returns a map with result sets by region.
+ * Such as:
+ * {
+ *   'us-east-1' => [ {region: 'us-east-1'}, greenRegion: false, blendedCost: 1},
+ *                    {region: 'us-east-1'}, greenRegion: false, blendedCost: 2}],
+ *   'eu-west-1' => [ {region: 'eu-west-'}, greenRegion: true, blendedCost: 1},
+ *   ]
+ * }
+ */
 async function getAssignedCost(raw) {
   const greenRegions = ['us-west-2','eu-central-1', 'eu-west-1','ca-central-1','us-gov-west-1'];
 
   let costPerRegion = [];
   raw.ResultsByTime.forEach((result) => {
     const costItem = result.Groups.map((data) => {
-      return { region: data.Keys, blendedCost: data.Metrics.BlendedCost.Amount, greenRegion: greenRegions.includes(data.Keys[0])};
+      return { region: data.Keys[0], blendedCost: Number.parseFloat(data.Metrics.BlendedCost.Amount), greenRegion: greenRegions.includes(data.Keys[0])};
     });
     costPerRegion = costPerRegion.concat(costItem);
   });
 
-  return costPerRegion;
+  const groupedCost = sumByRegion(costPerRegion);
+  return groupedCost;
 }
 
 async function runExplorer() {
@@ -58,5 +137,7 @@ async function runExplorer() {
 
 module.exports = {
   getRawCosts,
+  getAssignedCost,
+  getTotalCost,
   runExplorer
 }
